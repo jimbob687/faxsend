6a458930d0cdf971d9b39f1604ae5faa08c0098d
james.nieper@gmail.com <james.nieper@gmail.com>
2017-04-14 13:56:41 -0700

    Update

--- checktags.sh
+++ checktags.sh
@@ -12,7 +12,7 @@ echo $MYTAGS
 
 if [[ ${MYTAGS} != *"BUILT"* ]];then
   echo "Built is not in tags"
-  git tag -a $NEWTAG -m "have built version"
+  git tag -a BUILT-${DATENOW} -m "have built version"
   git push origin --tag
 else
   echo "Tag is already in commit"
