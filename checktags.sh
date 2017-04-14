6112184c98c73e6a36e2f2b18ea141a67bde67dc
james.nieper@gmail.com <james.nieper@gmail.com>
2017-04-14 13:55:06 -0700

    Update

--- checktags.sh
+++ checktags.sh
@@ -2,9 +2,18 @@
 
 MYTAGS=`git tag --points-at HEAD`
 
+DATENOW=`date +%s`
+echo "DATENOW-$DATENOW"
+echo "DATENOW-${DATENOW}"
+NEWTAG='BUILT-${DATENOW}'
+
 echo "Have run script"
 echo $MYTAGS
 
-if [[ ${MYTAGS} != *"built"* ]];then
+if [[ ${MYTAGS} != *"BUILT"* ]];then
   echo "Built is not in tags"
+  git tag -a $NEWTAG -m "have built version"
+  git push origin --tag
+else
+  echo "Tag is already in commit"
 fi
